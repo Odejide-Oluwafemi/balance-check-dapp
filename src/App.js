f0215a7b00e7ff95e3f6b3f117c0bf20e3cb3e60
LordDregg <oluwatosinodejide121@gmail.com>
2025-10-01 21:26:15 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { ethers } from "ethers";
 
 function App() {
+  const [balance, setBalance] = useState(0);
+  const [address, setAddress] = useState(null);
+  const [network, setNetwork] = useState(null);
+
+  const loadData = async () => {
+    if (typeof window.ethereum === "undefined") {
+      alert("Need to install a Crypto Wallet like Metamask!");
+      return;
+    }
+
+    if (address !== null)
+    {
+      alert(
+        "Doctor Segun's Wallet is already Connected! Run small USDT for boys na"
+      );
+      return;
+    }
+
+    try {
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const account = (
+        await window.ethereum.request({ method: "eth_requestAccounts" })
+      )[0];
+      const signer = await provider.getSigner(account);
+      const address = await signer.getAddress();
+      const balance = await provider.getBalance(address);
+      const network = await provider.getNetwork();
+
+      setAddress(address);
+      setBalance(ethers.formatEther(balance));
+      setNetwork(network.name);
+    } catch (error) {
+      console.error(error);
+      alert("Wallet Connection Rejected by Bro. Segun. Sorry, Doctor Segun🤭");
+    }
+    // const balance = await provider.getBalance(account);
+    // setAddress(ethers.getAddress(account));
+
+    // setProvider(provider);
+    // setAccount(account);
+    //
+
+    window.ethereum.on("accountsChanged", async () => {
+      window.location.reload();
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <header>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 320 512"
+          width="40px"
         >
-          Learn React
-        </a>
+          <path d="M311.9 260.8L160 353.6 8 260.8 160 0 311.9 260.8zM160 383.4L8 290.6 160 512 312 290.6 160 383.4z" />
+        </svg>
+        <h1>ETH Balance Check</h1>
       </header>
-    </div>
+
+      <main>
+        <div>
+        <button
+          className={
+            "big-round-button-container" +
+            " " +
+            (address !== null ? "connected" : "")
+          }
+          onClick={() => loadData()}
+        >
+          {address === null ? "Connect Wallet" : "Connected"}
+        </button>
+
+        {address !== null && (
+          <div>
+            <p>Network: {network}</p>
+            <p>Address: {address}</p>
+            <p>Balance: {balance} ETH</p>
+
+            <strong>Doctor Segun's Wallet is already Connected! Run small USDT for boys na</strong>
+          </div>
+        )}
+        </div>
+      </main>
+
+      <footer>
+        <p>
+          &copy;{new Date().getFullYear()} ElyteTechnologies. All Rights
+          Reserved.
+        </p>
+      </footer>
+    </>
   );
 }
 
